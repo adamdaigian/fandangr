5ef6d7ffa2ab9465644b13fe080c0758e9ae5d54
Adam Daigian <adam.daigian@gmail.com>
2013-07-06 11:40:17 -0700

    adding new rails fandangr app to github

---
+++ fandangr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fandangr::Application.config.secret_token = '5dfd7cf901e7e15ba31b2f748723b3957692838f2d4be68d5ad6c780fcf51faf37882684c3e0457c22c34e9d05f409d080a13e416a1bfa44c7e89ecfcc48325e'
